858ac69ee29688d54d75dbb031ea9357bf0c6ddf
gwaihir4 <bahadir.arabali@outlook.com>
2025-08-17 00:17:49 +0300

    added player, drawing, move

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from constants import *
-
+from player import Player
 def main():
-    print("Starting Asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                screen.fill("black")
-                pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return
+        player1.update(dt)
+        screen.fill("black")
+        player1.draw(screen)
+        pygame.display.flip()    
+        dt = game_clock.tick(60) / 1000
+        
+
+
 
 
 if __name__ == "__main__":
